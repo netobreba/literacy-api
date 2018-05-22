2876415a6f5da61deb28e5a64495f6826b566e84
netobreba <jose.silvaneto@dce.ufpb.br>
2018-05-21 21:29:34 -0300

    correção para deploy

--- src/database.js
+++ src/database.js
@@ -1,7 +1,14 @@
 import Sequelize from 'sequelize'
 
-export let sequelize = new Sequelize('literacy_db', null, null, {
+// export let sequelize = new Sequelize('literacy_db', null, null, {
+//     host: 'localhost',
+//     dialect: 'sqlite',
+//     storage: './data.sqlite'
+// })
+
+let sequelize = new Sequelize('literacy', process.env.DB_USER || 'postgres', process.env.DB_PASSWORD || 'neto1234', {
     host: 'localhost',
-    dialect: 'sqlite',
-    storage: './data.sqlite'
-})
+    port: '5432',
+    dialect: 'postgres',
+    operatorsAliases: false
+});
